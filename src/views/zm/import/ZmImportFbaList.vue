712ac73db69e80a27e6e7c7ca1701f21ea0796a6
zhuzhihong <846211162@qq.com>
2021-09-25 11:12:51 +0800

    运单拣货提示

--- src/views/zm/import/ZmImportFbaList.vue
+++ src/views/zm/import/ZmImportFbaList.vue
@@ -531,7 +531,9 @@
             content: 'When clicked the OK button, this dialog will be closed after 1 second',
             onOk() {
               return new Promise((resolve, reject) => {
-                setTimeout(Math.random() > 0.5 ? resolve : reject, 1000);
+                resolve(record);
+              }).then(function(value) {
+                console.log(value);
               }).catch(() => console.log('Oops errors!'));
             },
             onCancel() {},
